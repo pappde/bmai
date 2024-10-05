245121edb94cfbe45b219df202c0b271b6cfdda0
Dan Langford <721364+danlangford@users.noreply.github.com>
2024-10-04 19:21:28 -0600

    c++17, cmake 3.19, VS2022 (#10) (#58)
    * cmake 3.19, c++17
    
    * fixing windows build
    
    * cleanup extra token and some typos
    
    * fixup mac build
    
    * archive file names
    
    * GIT_DESCRIBE should match within binary
    
    * macosx universal binary
    
    * GIT_DESCRIBE with PR number 
    
    * bundle up two sample fights

--- test/BMAI3Tests.cpp
+++ test/BMAI3Tests.cpp
@@ -2,11 +2,8 @@
 #include "../src/bmai_ai.h"
 #include "../src/parser.h"
 #include <cstdarg>  // for va_start() va_end()
-
-// filesystem was weird in c++14 with various ways the operating systems juggled the experimental namespace
-// this util i found gives us c++17 filesystem patterns down in older c++
-#include <ghc/filesystem.hpp>
-namespace fs = ghc::filesystem;
+#include <filesystem>
+namespace fs = std::filesystem;
 
 // a path util, if useful consider creaking out into generic utility
 inline std::string resolvePath(const std::string &relPath)
@@ -32,44 +29,58 @@ inline std::string resolvePath(const std::string &relPath)
 // lets us collect what is being sent to Send for inspection during tests
 class TEST_Parser : public BMC_Parser {
 public:
-    void Send(const char *_fmt, ...) {
-        tm_next_to_last_fmt = tm_last_fmt;
-
+    void Send(const char *_fmt, ...) override {
         char buff[BMD_MAX_STRING];
         va_list ap;
         va_start (ap, _fmt);
         vsnprintf(buff, BMD_MAX_STRING, _fmt, ap);
         va_end (ap);
 
-        tm_last_fmt = std::string(buff);
+        tm_current_fmt = tm_current_fmt + std::string(buff);
+        if (tm_current_fmt.back()=='\n') {
+            tm_third_to_last_fmt = tm_next_to_last_fmt;
+            tm_next_to_last_fmt = tm_last_fmt;
+            tm_last_fmt = tm_current_fmt;
+            tm_current_fmt = "";
+        }
     }
+    std::string tm_third_to_last_fmt;
     std::string tm_next_to_last_fmt;
     std::string tm_last_fmt;
+    std::string tm_current_fmt;
 };
 
-class BMAISurrenderTests :public ::testing::TestWithParam<std::tuple<std::string, std::string>> {
+class BMAIActionTests :public ::testing::TestWithParam<std::tuple<std::string, std::string>> {
 protected:
     TEST_Parser parser;
 };
 
 // I wanted to test the smaller objects and methods. 
-// For example i could build an AI object and just execute the method GetAttackAction()
-// to inspect the behavior arround surrender
+// For example, I could build an AI object and just execute the method GetAttackAction()
+// to inspect the behavior around surrender
 // however the code is not currently built in a way that is friendly to unit testing
 // so the current pattern is to run full simulations and to monitor the output
 
 INSTANTIATE_TEST_SUITE_P(
         BMAI3Tests,
-        BMAISurrenderTests,
+        BMAIActionTests,
         ::testing::Values(
             std::make_tuple("test/SurrenderDefault-Pass-in.txt", "action\nsurrender\n"),
-            std::make_tuple("test/SurrenderOff-Attack-in.txt", "0\n0\n"),
+            std::make_tuple("test/SurrenderOff-Attack-in.txt", "power\n0\n0\n"),
             std::make_tuple("test/SurrenderOff-Pass-in.txt", "action\npass\n"),
             std::make_tuple("test/SurrenderOn-Attack-in.txt", "action\nsurrender\n"),
-            std::make_tuple("test/SurrenderOn-Pass-in.txt", "action\nsurrender\n")
+            std::make_tuple("test/SurrenderOn-Pass-in.txt", "action\nsurrender\n"),
+
+            std::make_tuple("test/bug55_a_in.txt", "skill\n2 0\n0\n"),
+            std::make_tuple("test/bug55_b_in.txt", "skill\n3 0 1\n2\n")
+
+            // LONG RUNNING! use for MANUAL REGRESSION
+            // TODO create better regression patterns that can be triggered from some builds
+            // , std::make_tuple("test/bug11_in.txt", "action\nswing T 4\nswing W 4\n")
+            // , std::make_tuple("test/bug16_in.txt", "action\nreserve 6\n")
         ));
 
-TEST_P(BMAISurrenderTests, CheckSurrenderAction){
+TEST_P(BMAIActionTests, CheckSurrenderAction){
     // Arrange Act Assert
     // is a parallel to the pattern
     // Given When Then
@@ -90,5 +101,5 @@ TEST_P(BMAISurrenderTests, CheckSurrenderAction){
 
     // Assert
     // Then the move selected should match our expectations
-    EXPECT_EQ(parser.tm_next_to_last_fmt+parser.tm_last_fmt, expected_last_actions);
+    EXPECT_EQ(parser.tm_third_to_last_fmt+parser.tm_next_to_last_fmt+parser.tm_last_fmt, expected_last_actions);
 }
